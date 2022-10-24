f123753d944e3fb533e060a39550d7094e827ec3
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-10-24 17:30:40 -0300

    feat(icons): adds authentication icons

---
+++ src/components/Icons/PersonCircle/index.tsx
@@ -0,0 +1,18 @@
+import { IIcon } from "../interfaces";
+
+const PersonCircle = ({ size, color }: IIcon) => {
+  return (
+    <svg
+      width={size || "1rem"}
+      height={size || "1rem"}
+      color={color || "#000"}
+      xmlns="http://www.w3.org/2000/svg" fill="none"
+      viewBox="0 0 16 16"
+    >
+      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+      <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+    </svg>
+  );
+};
+
+export { PersonCircle };
--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -13,3 +13,6 @@ export * from "./ExternalLinkIcon";
 export * from "./PaperClipIcon";
 export * from "./ArrowBackIcon";
 export * from "./ArrowDownIcon";
+export * from "./At";
+export * from "./PersonCircle";
+export * from "./KeyFill";
