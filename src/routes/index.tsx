53701798bbcda68f6e5ab9b5860816e82c0cbf0d
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-10-24 17:35:21 -0300

    feat: adds authentication routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 
+import Authentication from "../pages/Auth";
+
 const Routers = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Authentication page="login" />} />
+        <Route path="/register" element={<Authentication page="register" />} />
+        <Route path="/forget-password" element={<Authentication page="forget-password" />} />
       </Routes>
     </Router>
   );
