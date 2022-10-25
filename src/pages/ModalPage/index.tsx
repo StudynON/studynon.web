137b2d84e58daf0ae5b554dd5c0d7730d5fda037
felpfsf <feferreira.sf@gmail.com>
2022-10-25 15:27:03 -0300

    feat: preview page to show modal working

--- src/pages/ModalPage/index.tsx
+++ src/pages/ModalPage/index.tsx
@@ -9,22 +9,14 @@ const ModalPage = () => {
     <Wrapper>
       <button onClick={() => setActive(true)}>Abrir Modal</button>
 
-      {/* <Modal
-        title="Material Adicionado Com Sucesso"
-        subTitle="Em instantes você poderá ver as atualizações em suas pastas"
-        btnAddText="Adicionar Novo Material"
-        btnReturnText="Retornar Aos Materiais"
-        active={active}
-        hideModal={() => setActive(false)}
-      /> */}
-
       <Modal
         title="Meta adicionada com sucesso"
         subTitle="Em instantes você poderá ver as atualizações em seu calendário"
         btnAddText="Adicionar nova meta"
         btnReturnText="Retornar ao Cronograma"
         active={active}
-        hideModal={() => setActive(false)}
+        linkToAddNew='/modalpage'
+        linkToReturn='/'
       />
     </Wrapper>
   );
