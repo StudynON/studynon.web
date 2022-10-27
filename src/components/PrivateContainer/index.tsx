437b37fd79edb0a542918ddf1e2525f5cf453f53
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-10-27 16:52:26 -0300

    feat: add state for open and close menu mobile

--- src/components/PrivateContainer/index.tsx
+++ src/components/PrivateContainer/index.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
+import SidebarMobile from "../Sidebar/SidebarMobile";
 import { Container, ContentSection, Main } from "./style";
 
 const PrivateContainer = () => {
+  const [menuIsActive, setMenuIsActive] = useState(false);
+
+  const toggleToActive = () => {
+    setMenuIsActive(true);
+  };
+
+  const toggleToDisabled = () => {
+    setMenuIsActive(false);
+  };
+
   return (
     <Container>
-      <Header />
+      <Header openMenuMobile={toggleToActive} />
 
       <ContentSection>
-        <Sidebar />
+        {/* <Sidebar /> */}
+        <SidebarMobile
+          isActive={menuIsActive}
+          closeMenuMobile={toggleToDisabled}
+        />
 
         <Main>
           <Outlet />
