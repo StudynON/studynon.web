b34dc1bef810500149d0f45b08dd3096e55dfa2e
felpfsf <feferreira.sf@gmail.com>
2022-10-31 18:15:01 -0300

    refactor: added sandbox route to test components

---
+++ src/pages/Sandbox/index.tsx
@@ -0,0 +1,13 @@
+import FormAddMaterial from "../../components/AddMaterialForm";
+import { Wrapper } from "./style";
+
+const SandboxTest = () => {
+
+  return (
+    <Wrapper>
+      <FormAddMaterial />
+    </Wrapper>
+  );
+};
+
+export default SandboxTest;
