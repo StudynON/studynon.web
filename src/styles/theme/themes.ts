2e7a578cfc79c7a32609b27c93631aae4f7da526
felpfsf <feferreira.sf@gmail.com>
2022-11-01 12:24:26 -0300

    refactor: refac breakpoints and devices

--- src/styles/theme/themes.ts
+++ src/styles/theme/themes.ts
@@ -1,32 +1,28 @@
 import { IDefaultThemeProps, IBreakpoints, IThemeProps } from "./";
 
 const breakpoints = {
-  breakpoints: {
-    mobileS: "320px",
-    mobileM: "375px",
-    mobileL: "425px",
-    tablet: "768px",
-    laptop: "1024px",
-    desktop: "1440px",
-    desktopL: "2560px",
-  },
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  desktop: "1440px",
+  desktopL: "2560px",
 };
 
 const devices = {
-  devices: {
-    mobileS: `(min-width: ${breakpoints.breakpoints.mobileS})`,
-    mobileM: `(min-width: ${breakpoints.breakpoints.mobileM})`,
-    mobileL: `(min-width: ${breakpoints.breakpoints.mobileL})`,
-    tablet: `(min-width: ${breakpoints.breakpoints.tablet})`,
-    laptop: `(min-width: ${breakpoints.breakpoints.laptop})`,
-    desktop: `(min-width: ${breakpoints.breakpoints.desktop})`,
-    desktopL: `(min-width: ${breakpoints.breakpoints.desktopL})`,
-  },
+  mobileS: `(min-width: ${breakpoints.mobileS})`,
+  mobileM: `(min-width: ${breakpoints.mobileM})`,
+  mobileL: `(min-width: ${breakpoints.mobileL})`,
+  tablet: `(min-width: ${breakpoints.tablet})`,
+  laptop: `(min-width: ${breakpoints.laptop})`,
+  desktop: `(min-width: ${breakpoints.desktop})`,
+  desktopL: `(min-width: ${breakpoints.desktopL})`,
 };
 
 const globalBreakpoints: IBreakpoints = {
-  ...breakpoints,
-  ...devices,
+  breakpoints,
+  devices,
 };
 
 const globalColors: IDefaultThemeProps = {
