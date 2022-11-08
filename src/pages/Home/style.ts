6c0d6c10b438fd36a3f8a6fb384340308ccf1b3b
Felipe F <feferreira.sf@gmail.com>
2022-11-08 16:51:03 -0300

    style: dimensions

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -7,20 +7,19 @@ interface IProps {
 }
 
 const ContainerHome = styled.div`
-  max-width: 1154px;
+  max-width: 1200px;
+  min-width: 20.3125rem;
+  width: 100%;
+
   margin: 3.75rem auto;
+  
+  padding: 1rem;
 `;
 
 const TitleHome = styled.h1`
   font-size: clamp(1.5rem, 1.2733rem + 1.0076vw, 2rem);
   font-weight: 600;
   color: ${({ theme }: IProps) => theme.darkKettle};
-
-  margin-left: 2rem;
-
-  @media ${({ theme }: IProps) => theme.devices.desktop} {
-    margin: auto;
-  }
 `;
 
 export { ContainerHome, TitleHome };
